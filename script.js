6828a79c75fc37852a52434e7353a35498bd8b07
Sudais Ahmad Khan <sudaisahmadkhan8@gmail.com>
2025-08-19 14:02:39 +0500

    some chnages has been made

--- script.js
+++ script.js
@@ -44,14 +44,12 @@ let changePage = function(page){
             el.classList.remove("load");
         }
         if(!el.classList.contains("hidden")){
-            let id = el.id;
             menuButtons.forEach(function(elem){
                 elem.classList.remove("main-icon");
                 elem.style.color = "var(--color-tertiary)";
                 elem.firstElementChild.style.fill = "var(--color-tertiary)";
             });
             menuButtons.forEach(function(e){
-                console.log(e.textContent);
                 if(e.lastElementChild.textContent == el.id){
                     e.classList.add("main-icon");
                     e.style.color = "var(--color-body-text)";
@@ -76,3 +74,50 @@ menuButton.forEach(function(e){
 
     })
 });
+let neutralize = function(){
+    menuButtons.forEach(function(elem){
+        elem.classList.remove("main-icon");
+        elem.style.color = "var(--color-tertiary)";
+        elem.firstElementChild.style.fill = "var(--color-tertiary)";
+    });
+    pages.forEach(function(page){
+        page.classList.add("hidden");
+        page.classList.remove("load")
+    })
+}
+let openService = function(){
+    neutralize();
+    let e = menuButtons[3];
+    e.classList.add("main-icon");
+    e.style.color = "var(--color-body-text)";
+    e.firstElementChild.style.fill = "var(--color-secondary)";
+    pages[3].classList.remove("hidden");
+    pages[3].classList.add("load")
+}
+let openContact = function(){
+    neutralize();
+    let e = menuButtons[5];
+    e.classList.add("main-icon");
+    e.style.color = "var(--color-body-text)";
+    e.firstElementChild.style.fill = "var(--color-secondary)";
+    pages[5].classList.remove("hidden");
+    pages[5].classList.add("load")
+}
+let openProject = function(){
+    neutralize();
+    let e = menuButtons[2];
+    e.classList.add("main-icon");
+    e.style.color = "var(--color-body-text)";
+    e.firstElementChild.style.fill = "var(--color-secondary)";
+    pages[2].classList.remove("hidden");
+    pages[2].classList.add("load")
+}
+let openAbout = function(){
+    neutralize();
+    let e = menuButtons[1];
+    e.classList.add("main-icon");
+    e.style.color = "var(--color-body-text)";
+    e.firstElementChild.style.fill = "var(--color-secondary)";
+    pages[1].classList.remove("hidden");
+    pages[1].classList.add("load")
+}
